68060bcff356a09fe9a5df0272b878e85992297b
Elias Norrby <elias.norrby@gmail.com>
2020-05-14 23:42:50 +0200

    L2.4: Mutable and Immutable Types

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-students_count = 1000
-rating = 4.99
-is_published = False
-course_name = "Python"
-lines = """
-Multiple
-Lines
-"""
-x, y = 1, 2
-a = b = 1
+x = [1,2,3]
+print(id(x))
+
+x.append(4)
+print(id(x))
+
