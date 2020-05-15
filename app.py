53f34ae2f1db182743ac608c2389c677ae581624
Elias Norrby <elias.norrby@gmail.com>
2020-05-15 10:20:38 +0200

    L2.20: Arguments - xargs

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-# Functions
-def increment(number: int, by: int = 1) -> tuple:
-    return (number, number + by)
+# Arguments
+def multiply(*list):
+    total = 1
+    for number in list:
+        total *= number
+    return total
 
 
-print(increment(2, 3))
+print(multiply(2, 3, 4, 5))
 
-numbers = [1, 2, 3]  # list
-number_tuple = (1, 2, 3)  # immutable
 
-# keyword arguments
-print(increment(2, by=3))
 
-# defult parameters
-print(increment(2))
+
+
+
+
+
+
