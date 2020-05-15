67b47b43a31c09c248251f36d4baef4d5239e14d
Elias Norrby <elias.norrby@gmail.com>
2020-05-15 09:39:31 +0200

    L2.14: Logical Operators

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-# Conditional Statements
-age = 22
-if age >= 18:
-    print("Adult")
-    print("Adult")
-elif age >= 13:
-    print("Teenager")
-else:
-    print("Child")
+# Logical Operators
+name = " "
 
-print("All done!")
+if not name.strip():
+    print("Name is empty")
+
+age = 22
+if age >= 18 and age < 65:
+    print("Eligible")
+
+# Clean syntax!
+if 18 <= age < 65:
+    print("Eligible")
