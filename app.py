d6cabe06d24776fb84ee739dd2c0a3cbbf188849
Elias Norrby <elias.norrby@gmail.com>
2020-05-15 11:23:34 +0200

    L2.26: FizzBuzz

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-# Scope
-message = "a"
+# FizzBuzz
+def fizz_buzz(input):
+    result = ""
+    if input % 3 == 0:
+        result += "Fizz"
+    if input % 5 == 0:
+        result += "Buzz"
+    return result or input
 
-def greet():
-    global message
-    message = "b"
-    print(message)
 
-greet()
-print(message)
+# print(fizz_buzz(1))
+# print(fizz_buzz(3))
+# print(fizz_buzz(5))
+# print(fizz_buzz(15))
 
+for number in range(0, 101):
+    print(fizz_buzz(number))
