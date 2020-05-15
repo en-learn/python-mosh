5e8264089453b6db5d9f4e67413250bb0771c20f
Elias Norrby <elias.norrby@gmail.com>
2020-05-15 10:09:46 +0200

    L2.19: Functions

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-# While Loops
-guess = 0
-answer = 5
+# Functions
+def increment(number: int, by: int = 1) -> tuple:
+    return (number, number + by)
 
-while answer != guess:
-    guess = int(input("Guess: "))
-else:
-    print("No break statement was used")
+
+print(increment(2, 3))
+
+numbers = [1, 2, 3]  # list
+number_tuple = (1, 2, 3)  # immutable
+
+# keyword arguments
+print(increment(2, by=3))
+
+# defult parameters
+print(increment(2))
