68c161507652ea6dcb8807d9a5ff5c098e6f42cf
Elias Norrby <elias.norrby@gmail.com>
2020-05-15 09:48:50 +0200

    L2.16: For Loops

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-# Logical Operators
-age = 22
+# For Loops
+# for x in "Python":
+#     print(x)
 
-if age >= 18:
-    message = "Eligible"
-else:
-    message = "Not eligible"
+# for x in ['a', 'b', 'c']:
+#     print(x)
 
-message = "Eligible" if age >= 18 else "Not eligible"
+for x in range(5):
+    print(x)
 
-print(message)
+print("")
+
+for x in range(3, 5):
+    print(x)
+
+print("")
+
+for x in range(0, 10, 2):
+    print(x)
+
+# The range function does not return a list object
+print(range(5))
+print([1, 2, 3, 4, 5])
+
+# A range takes very little memory, even if the range is large
+print(type(range(5000000000)))
